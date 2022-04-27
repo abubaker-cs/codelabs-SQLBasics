e36c939a8a9572f6e4219256f2fc89bf1ed00a88
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-04-28 04:41:04 +0500

    Preparing files for the project.

--- app/src/main/java/com/example/sqlbasics/AppDatabase.kt
+++ app/src/main/java/com/example/sqlbasics/AppDatabase.kt
@@ -21,7 +21,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = arrayOf(CaliforniaPark::class), version = 1)
-abstract class AppDatabase: RoomDatabase() {
+abstract class AppDatabase : RoomDatabase() {
     abstract fun californiaParkDao(): CaliforniaParkDao
 
     companion object {
